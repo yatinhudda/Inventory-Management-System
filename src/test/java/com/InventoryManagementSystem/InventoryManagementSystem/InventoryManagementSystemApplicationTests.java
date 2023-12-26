7b339c3eee0ed91bf23e3dc8e1f3537032682469
yatinhudda <yatinchaudhary72@gmail.com>
2023-12-26 19:20:10 +0530

    Initialize the folder

---
+++ src/test/java/com/InventoryManagementSystem/InventoryManagementSystem/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InventoryManagementSystem.InventoryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
